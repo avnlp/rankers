1e7df356579080b46741e6421a68c12ae519e131
awinml <97467100+awinml@users.noreply.github.com>
2025-02-06 16:49:04 +0530

    Add Setwise LLM Ranker to high level import

--- src/rankers/__init__.py
+++ src/rankers/__init__.py
@@ -1,5 +1,6 @@
 from rankers.dataloader.dataloader import Dataloader
 from rankers.evaluation.evaluator import Evaluator
 from rankers.evaluation.evaluator_config import EvaluatorConfig
+from rankers.setwise.setwise_ranker import SetwiseLLMRanker
 
-__all__ = ["Dataloader", "Evaluator", "EvaluatorConfig"]
+__all__ = ["Dataloader", "Evaluator", "EvaluatorConfig", "SetwiseLLMRanker"]
